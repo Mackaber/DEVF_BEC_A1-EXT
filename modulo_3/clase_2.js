b5280283d01c01d934c034ad1ecf07d422569623
Mackaber <x@mackaber.me>
2025-02-06 19:12:57 -0600

    clase jueves

---
+++ modulo_3/clase_2.js
@@ -0,0 +1,25 @@
+var a = "palabra"
+var b = 42
+
+var a = "otra cosa"
+
+
+// ----------------------
+
+let l = "esta"
+
+// Esto no va a funcionar, porque solo se puede declarar una vez
+// let l = "esta otra"
+
+l = "esta otra" 
+l = 5
+
+// ---------------------------
+
+const c = "constante"
+//const c = "variable" Esta no funcionará
+//c = "variable" Esta tampoco
+
+console.log(c)
+
+
