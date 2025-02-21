a9e2a79364495811517ac6647194fb570cf17cf1
Mackaber <x@mackaber.me>
2025-02-20 20:25:19 -0600

    clase jueves

--- modulo_3/clase_6/manejo_inputs/script.js
+++ modulo_3/clase_6/manejo_inputs/script.js
@@ -1,7 +1,19 @@
 let button = document.getElementById("button")
+let result = document.getElementById("result")
 
 // Add event listener
 
 button.addEventListener("click", () => {
     console.log("Boton presionado!")
+
+    console.log("El nombre del usuario es: "  + nombre.value)
+
+    result.innerHTML = "Bienvenido " + nombre.value
 })
+
+
+// Acceder a los inputs
+
+let nombre = document.getElementById("nombre")
+let email = document.getElementById("email")
+
