3ce91e9eb5e4d2d1c893d6a51b640c5f1d87f9cc
Mackaber <x@mackaber.me>
2025-04-03 21:28:18 -0600

    Clase Jueves

---
+++ modulo_4/clase_6/permute.js
@@ -0,0 +1,17 @@
+// Permute
+
+function permute (str, path = '', result = [])  {
+    if(str.length === 0) {
+        result.push(path);
+    } else {
+        for(let i=0; i< str.length; i++) {
+            const remaining = str.slice(0,i) + str.slice(i + 1)
+            console.log(remaining);
+            permute(remaining, path + str[i], result);
+        }
+    }
+    return result;
+}
+
+const result = permute('abcde').length;
+console.log(result);
---
+++ modulo_4/clase_6/reto.js
@@ -0,0 +1,15 @@
+// Fibonacci
+function fibo(n) {
+    // Caso(s) base
+    if(n === 0) {
+        return 0
+    }
+    // Llamada recursiva
+    return ??? + fibo(n-1)
+
+}
+
+
+// Primera llamada
+let result = fibo(5) // 8
+console.log(result)
