e9c2a9ab2a1fecba4adbf338f72eeb2d16785429
Mackaber <x@mackaber.me>
2025-05-06 20:47:25 -0600

    Clase Martes

---
+++ modulo_5/clase_2/promesas.js
@@ -0,0 +1,27 @@
+const promesa = new Promise((resolve, reject) => {
+    // Llamar al API
+    // Llamar a la db
+    // Leer Archivos...
+    resolve("data")
+    //reject("error")
+})
+
+// Usando then  
+
+promesa.then((response) => {
+    console.log(response)
+    return response.substring(1)
+}).then((response) => {
+    return response.substring(1)
+}).then((response) => {
+    return response.substring(1)
+}).then((response) => {
+    console.log(`Response, pero en el 4to then: ${response}`)
+}).catch((error) => {
+    console.log(error)
+})
+
+// Usando Async/Await
+
+let response = await promesa
+console.log(response)
