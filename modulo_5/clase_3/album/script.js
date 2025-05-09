584d70dafe17c374f59402f9f97564e1e46cc41d
Mackaber <x@mackaber.me>
2025-05-08 20:54:48 -0600

    formulario de envio listo

--- modulo_5/clase_3/album/script.js
+++ modulo_5/clase_3/album/script.js
@@ -1,4 +1,5 @@
 const imagesContainer = document.getElementById("images-container")
+const uploadForm = document.getElementById("upload-form")
 const renderImage = (imageObject) => {
     return `
         <div>
@@ -11,15 +12,44 @@ const requestOptions = {
     method: "GET",
     redirect: "follow"
 };
-  
-fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", requestOptions)
-    .then((response) => response.json())
-    .then((images) => {
-        let html = ""
-        for(let i = 0; i<images.length; i++) {
-            html += renderImage(images[i])
-        }
-        console.log(html)
-        imagesContainer.innerHTML = html
+
+const loadImages = () => {
+    fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", requestOptions)
+        .then((response) => response.json())
+        .then((images) => {
+            let html = ""
+            for(let i = 0; i<images.length; i++) {
+                html += renderImage(images[i])
+            }
+            console.log(html)
+            imagesContainer.innerHTML = html
+        })
+        .catch((error) => console.error(error));
+}
+
+uploadForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    console.log(e.target.title.value)
+
+    const raw = JSON.stringify({
+        id: "=ROW()",
+        titulo: e.target.title.value
     })
-    .catch((error) => console.error(error));
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", {
+        headers: myHeaders,
+        method: "POST",
+        body: raw
+    }).then((response) => response.json())
+    .then((result) => {
+        console.log(result)
+        loadImages()  
+    })
+    .catch((error) => console.error(error))
+})
+
+loadImages()
